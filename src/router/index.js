c3eda681cba19f33fd91aa60196b39f0f88d1601
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2023-12-13 18:35:45 +0500

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: import('@/views/HomeView/index.vue')
+      component: () => import('@/views/HomeView/index.vue')
     },
   ]
 })
